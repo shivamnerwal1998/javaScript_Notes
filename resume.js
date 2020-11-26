5a7a4be54519fcfe745a3ceb13730aac22aadc7e
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-11-26 10:09:04 +0530

    addind add more and remove functionallity

--- resume.js
+++ resume.js
@@ -1,7 +1,33 @@
-
-function createMore(id)
+var i = 0 ;  
+function createMore()
 {
-    let data = document.getElementById(id) ;
-  
+    i++ ; 
+    let data = document.getElementById("educationDetails") ;
+    let element = document.createElement("textarea");
+    let linebreak = document.createElement("br") ;
+    element.id = "educationDetails" + i ; 
+    element.rows = 2 ;
+    element.cols = 30 ;
+    data.appendChild(element);
+    let toremove = document.getElementById("educationDetails" + i) ;
     
+}
+function remove()
+{
+    let target = document.getElementById("educationDetails") ;
+    
+    try
+    {
+        let target = document.getElementById("educationDetails") ;
+        let element = document.getElementById("educationDetails"+i) ;
+        target.removeChild(element) ;     
+        i-- ;   
+    
+    }
+    catch
+    {
+        alert("nothing to delete ") ; 
+
+    } 
+
 }
