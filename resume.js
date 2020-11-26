b1e678f2575005420dfb524dbb56ec74cf19bb37
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-11-27 01:20:55 +0530

    udated js

--- resume.js
+++ resume.js
@@ -1,33 +1,50 @@
-var i = 0 ;  
-function createMore()
+var count = 0 ; 
+  
+function addDetails()
 {
-    i++ ; 
-    let data = document.getElementById("educationDetails") ;
-    let element = document.createElement("textarea");
-    let linebreak = document.createElement("br") ;
-    element.id = "educationDetails" + i ; 
-    element.rows = 2 ;
-    element.cols = 30 ;
-    data.appendChild(element);
-    let toremove = document.getElementById("educationDetails" + i) ;
+    count++ ; 
+    let target  = document.getElementById("education");
+    let element = document.createElement("tr");
+    element.id = "education"+ count ;
+    for(let i = 0 ; i<= 4 ; i++ ){
+        let feild  = document.createElement("td") ;
+        let details = document.createElement("input") ;
+        details.setAttribute("type" , "text") ;
+        details.placeholder = "Enter data" ;
+        feild.appendChild(details);
+        element.appendChild(feild);
+        target.appendChild(element);
+    } 
+
+}
+
+
+function removeDetails()
+{
+    let target = document.getElementById("education");
+    try{
+        let element  = document.getElementById("education"+count);
+        target.removeChild(element);
+        count-- ;
+    }
+    catch{
+        alert("Nothing to delete");
+        return;
+    }
+
     
 }
-function remove()
+function downloadPdf()
 {
-    let target = document.getElementById("educationDetails") ;
-    
-    try
-    {
-        let target = document.getElementById("educationDetails") ;
-        let element = document.getElementById("educationDetails"+i) ;
-        target.removeChild(element) ;     
-        i-- ;   
-    
-    }
-    catch
-    {
-        alert("nothing to delete ") ; 
-
-    } 
+    let doc = new jsPDF();
+    doc.setFontType("bold");
+    doc.text(20,20,document.getElementById("name").value);
+    doc.setFontType("normal");
+    doc.setFontSize(12);
+    doc.text(20,20,"Date of birth:"+document.getElementById("dob").value);
+    doc.text(20,30,"Father's Name"+document.getElementById("fName").value);
+    doc.text(20,48,"contact number:"+document.getElementById("cont").value);
+    doc.text(20,36,"Email:"+document.getElementById("Email").value);
+    doc.save("cv.pdf");
 
 }
